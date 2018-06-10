e3d6c00eb975321d23366815be8d71a4e9afc739
Illiamalashchuk <malashchuk93@gmail.com>
2018-06-11 02:09:59 +0300

    Last commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
 import './index.css';
 import App from './components/App';
+import { Route, BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+render(
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={App} />
+      </div>
+    </BrowserRouter>,
+  document.getElementById('root')
+)
