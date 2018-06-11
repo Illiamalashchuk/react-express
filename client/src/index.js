dbc257100fe9adfd38be6a2d31590ab80f6745b9
Illiamalashchuk <malashchuk93@gmail.com>
2018-06-11 04:26:58 +0300

    last

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from 'react-dom'
 import './index.css';
 import App from './components/App';
+import { Switch, Route } from 'react-router-dom';
 
 render(
-    <App/>,
+    <Switch>
+      <Route exact path='/' component={App}/>
+    </Switch>,
   document.getElementById('root')
 )
